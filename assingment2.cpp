656b9cd7f2d95a86fab699262a77fc6db410c439
rohith <thefirstredrays@gmail.com>
2026-09-13 13:13:35 +0530

    Add files via upload

---
+++ assingment2.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+   int F;
+   float c;
+   printf("Enter fahrenhiet value");
+   scanf("%d",&F);
+   c=5.0 / 9.0 * (F-32);
+   printf("Celcius=%.2f",c);
+   return 0;
+}
+    
